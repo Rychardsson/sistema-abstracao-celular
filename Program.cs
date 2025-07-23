3ec974978ce9576daa68a0e99079f4b4c3672384
Rychardsson <rychardssong2@gmail.com>
2025-07-22 22:08:49 -0300

    Implement tests for Nokia and iPhone classes in Main method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Testando o Nokia:");
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111111111111", 64);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\n-------------------------\n");
+
+        Console.WriteLine("Testando o iPhone:");
+        Smartphone iphone = new Iphone("789012", "iPhone 13", "222222222222222", 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
